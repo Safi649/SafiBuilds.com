f52029152c3c1b326c11b662414e1c6902040269
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 13:19:01 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 // 📁 pages/_app.js
-import '@/styles/globals.css'   // ✅ Import Tailwind styles
 
-// This is the custom App component in Next.js
-// It wraps all pages with global providers, styles, etc.
+import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      {/* You can add global components like Navbar/Footer here if needed */}
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
