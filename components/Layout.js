e63808761c30628b662b816778914573d14fac93
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 13:01:58 +0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,31 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+// 📁 components/Layout.js
+import Link from 'next/link'
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-1">{children}</main>
-      <Footer />
+      {/* Navbar */}
+      <nav className="bg-blue-600 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">🌍 Global Scholarships</h1>
+          <div className="space-x-4">
+            <Link href="/">Home</Link>
+            <Link href="/scholarships">Scholarships</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* Page Content */}
+      <main className="flex-grow container mx-auto p-6">
+        {children}
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-900 text-gray-300 py-4 text-center">
+        <p>© {new Date().getFullYear()} Global Scholarships. All rights reserved.</p>
+      </footer>
     </div>
-  );
+  )
 }
