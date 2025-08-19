e252d1da5983fc89303117c9019ebcffa7ec170d
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 10:50:45 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,11 @@
-import '../styles/globals.css'
-import { AnimatePresence, motion } from 'framer-motion'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import "../styles/globals.css"
+import Layout from "../components/Layout"
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Navbar />
-      <AnimatePresence mode='wait'>
-        <motion.div
-          key={router.route}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-      <Footer />
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
