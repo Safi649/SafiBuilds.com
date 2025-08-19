78c41a4f7995c8a3476c47099facd9d24b6e75f6
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 11:15:35 +0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="min-h-screen">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
