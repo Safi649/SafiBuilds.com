6e13b6f015fde0b39a3cb3f4d88f2e42593af275
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 13:01:10 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "../styles/globals.css"
-import Layout from "../components/Layout"
+// 📁 pages/_app.js
+import '../styles/globals.css'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
