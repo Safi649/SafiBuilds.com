def366649cdf1fef06a631fdec69a131679a087d
Safi649 <muhammadabbassafi332@gmail.com>
2025-08-19 13:10:16 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+// 📁 tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // if you use Next.js 13+ app router
   ],
   theme: {
     extend: {},
